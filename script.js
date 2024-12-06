3c6012653f14cb482e01ef141cb8b5237edcab69
Aastha-Rathi <aastharathi0404gmail.com>
2024-12-06 13:41:47 +0530

    index updated code

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+const navLinks = document.querySelectorAll('header nav a');
+
+
+
 const resumeBtns = document.querySelectorAll('.resume-btn');
 
-resumeBtn.forEach((btn, idx) => {
-    btn.addEventListner('click', () => {
-        const resumeDetails = document.querySelectorAll('.resume-detail');
-
-        resumeBtns.forEach(btn => {
-            btn.classList.remove('active');
+resumeBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        resumeBtns.forEach((otherBtn) => {
+            otherBtn.classList.remove('active');
         });
         btn.classList.add('active');
+
+        const detailId = btn.getAttribute('data-detail');
+        const resumeDetails = document.querySelectorAll('.resume-detail');
 
         resumeDetails.forEach(detail => {
             detail.classList.remove('active');
         });
-        resumeDetails[idx].classList.add('active');
+        document.querySelector(`.resume-detail.${detailId}`).classList.add('active');
     });
 });
